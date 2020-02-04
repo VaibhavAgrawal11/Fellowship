5e36f387238d671512b8b8549e08a1da21d7f9b9
unknown <vaibhavagrawal1110@gmail.com>
2020-02-04 12:16:39 +0530

    Initial commit.

---
+++ Input.java
@@ -0,0 +1,12 @@
+package basicCore;
+
+import java.util.Scanner;
+
+public class Input {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		System.out.println("Hello " +s+" How are you?" );
+	}
+
+}
